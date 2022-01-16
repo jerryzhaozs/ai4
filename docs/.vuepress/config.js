772d855d64ab74e0b79f6424a825b7a56a6d0114
赵孜硕 <jerryzhao2002@126.com>
2022-01-16 18:02:51 +0800

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/assets/img/logo.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: './', // 这是部署到github相关的配置
+  base: '/', // 这是部署到github相关的配置
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
