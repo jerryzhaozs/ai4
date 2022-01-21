24577583be88b9b8fc8dcaae5ef1989736e89246
赵孜硕 <jerryzhao2002@126.com>
2022-01-21 21:47:32 +0800

    guide_moved

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,8 +1,8 @@
 module.exports=[
     {text: '首 页', link: '/' },
-    {text: '导 航',link: '/guide/'},
     {text: '数 据', link: '/rsrc/' },
     {text: 'AI4AGR', link: '/res/'},
     {text: '学习资源',link: '/mater/'},
+    {text: '导 航',link: '/guide/'},
     {text: '关 于',link: '/about/'},
 ]
--- docs/.vuepress/config/nav_en.js
+++ docs/.vuepress/config/nav_en.js
@@ -1,8 +1,8 @@
 module.exports=[
     {text: 'Home', link: '/en/' },
-    {text: 'Guide',link: '/en/guide/'},
     {text: 'Resource', link: '/en/rsrc/' },
     {text: 'Research', link: '/en/res/'},
     {text: 'Material',link: '/en/mater/'},
+    {text: 'Guide',link: '/en/guide/'},
     {text: 'About',link: '/en/about/'},
 ]
