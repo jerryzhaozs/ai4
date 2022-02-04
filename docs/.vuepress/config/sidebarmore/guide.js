00432012ba9ff010c8fb45d6eb8d7017c372b05c
赵孜硕 <jerryzhao2002@126.com>
2022-02-05 00:00:56 +0800

    dataset

--- docs/.vuepress/config/sidebarmore/guide.js
+++ docs/.vuepress/config/sidebarmore/guide.js
@@ -6,8 +6,8 @@ module.exports = [
         sidebarDepth: 2,      // 可选的, 默认值是 1
         initialOpenGroupIndex: 1,
         children:[
-            {title: 'AI Blog',path:'/guide/aiblog'},
-            {title: '数据集',path:'/guide/dataset'},
+            {title: 'AI Blog',path:'/guide/aiblog/'},
+            {title: '数据集',path:'/guide/dataset/'},
             {title: '平台/框架',path:'/'},
             {title: '编程',path:'/'},
             {title: '地图类',path:'/'},
